bbae7c6844b38bce9e81ea4437cd22abe4654bfd
raultocantins <alexbraul.ar@gmail.com>
2021-03-18 10:16:19 -0300

    Refactoring Api with new features

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+/**
+ * Database setup
+ */
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.use(require("./src/routes"));
+
+app.listen(3000);
